15c2088e3c7f7e908c959a196df916575af324f5
root <duaehdtjs20@gmail.com>
2021-08-01 20:08:16 +0900

    switch

--- main.go
+++ main.go
@@ -2,23 +2,49 @@ package main
 
 import "fmt"
 
-func main() {
+type Direction int
 
-	temp := 30
-	rain := 40
+const (
+	None Direction = iota
+	North
+	East
+	South
+	West
+)
 
-	if temp >= 25 {
-		if rain >= 80 {
-			fmt.Println("덥고 비가 옵니다.")
-		} else if rain >= 20 {
-			fmt.Println("덥고 습합니다.")
-		} else {
-			fmt.Println("야외 활동하기 좋습니다.")
-		}
-	} else if temp < 10 || rain >= 80 {
-		fmt.Println("야외 활동하기 좋지 않습니다.")
-	} else {
-		fmt.Println("좋은 날씨입니다.")
+func DiretionToString(d Direction) string {
+	switch d {
+	case North:
+		return "North"
+	case East:
+		return "East"
+	case South:
+		return "South"
+	case West:
+		return "West"
+	default:
+		return "None"
 	}
+}
 
+func GetDirection(angle float64) Direction {
+	switch {
+	case angle >= 315, angle >= 0 && angle < 45:
+		return North
+	case angle >= 45 && angle < 135:
+		return East
+	case angle >= 135 && angle < 225:
+		return South
+	case angle >= 225 && angle < 315:
+		return West
+	default:
+		return None
+	}
+}
+
+func main() {
+	fmt.Println(DiretionToString(GetDirection(38.3)))
+	fmt.Println(DiretionToString(GetDirection(235.8)))
+	fmt.Println(DiretionToString(GetDirection(94.2)))
+	fmt.Println(DiretionToString(GetDirection(-30)))
 }
