ccc5c46fa4dcc7fc95b32e82ec3879baa73e7fe0
root <duaehdtjs20@gmail.com>
2021-08-01 19:32:42 +0900

    if

--- main.go
+++ main.go
@@ -2,18 +2,23 @@ package main
 
 import "fmt"
 
-const (
-	C1 = iota
-	C2
-	C3
-)
-
-const (
-	B1 = iota + 1
-	B2
-	B3
-)
-
 func main() {
-	fmt.Println(C3, B3)
+
+	temp := 30
+	rain := 40
+
+	if temp >= 25 {
+		if rain >= 80 {
+			fmt.Println("덥고 비가 옵니다.")
+		} else if rain >= 20 {
+			fmt.Println("덥고 습합니다.")
+		} else {
+			fmt.Println("야외 활동하기 좋습니다.")
+		}
+	} else if temp < 10 || rain >= 80 {
+		fmt.Println("야외 활동하기 좋지 않습니다.")
+	} else {
+		fmt.Println("좋은 날씨입니다.")
+	}
+
 }
