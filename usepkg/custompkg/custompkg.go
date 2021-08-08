6ccce6385740fc781cb75a829130d604eca6d6e0
root <duaehdtjs20@gmail.com>
2021-08-08 10:37:18 +0900

    package - init

--- usepkg/custompkg/custompkg.go
+++ usepkg/custompkg/custompkg.go
@@ -2,6 +2,22 @@ package custompkg
 
 import "fmt"
 
+type Student struct {
+	Name  string
+	Age   int
+	score int
+}
+
+var Ratio int
+var ttt int
+
+const PI = 3.14
+const pI2 = 3.1415
+
 func PrintCustom() {
+	fmt.Println("This is custom package!")
+}
+
+func printcustom2() {
 	fmt.Println("This is custom package!")
 }
--- usepkg/program/usepkg.go
+++ usepkg/program/usepkg.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"tuckerGo/usepkg/custompkg"
+	"tuckerGo/usepkg/exinit"
 
 	"github.com/guptarohit/asciigraph"
 	"github.com/tuckersGo/musthaveGo/ch16/expkg"
@@ -10,10 +11,21 @@ import (
 
 func main() {
 	custompkg.PrintCustom()
+
+	s := custompkg.Student{}
+	s.Name = "화랑"
+	s.Age = 32
+	fmt.Println(s.Name, s.Age)
+	fmt.Println(custompkg.PI)
+	custompkg.Ratio = 10
+
+	fmt.Println(custompkg.Ratio)
 	expkg.PrintSample()
 
 	data := []float64{3, 4, 5, 6, 9, 7, 1, 2, 3, 10, 4, 6, 5, 3, 1, 2, 9}
 
 	graph := asciigraph.Plot(data)
 	fmt.Println(graph)
+
+	exinit.PrintD()
 }
