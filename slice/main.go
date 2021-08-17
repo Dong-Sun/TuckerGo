16eaece4f8be7b80121e78d88f3cc652e06f82ff
root <duaehdtjs20@gmail.com>
2021-08-17 20:03:45 +0900

    slice - part 1

--- slice/main.go
+++ slice/main.go
@@ -2,12 +2,21 @@ package main
 
 import "fmt"
 
-func main() {
-	var slice []int
+func changeArray(array2 [5]int) {
+	array2[2] = 200
+}
 
-	if len(slice) == 0 {
-		fmt.Println("slice is empty", slice)
-	}
-	slice[1] = 10
+func changeSlice(slice2 []int) {
+	slice2[2] = 200
+}
+
+func main() {
+	array := [5]int{1, 2, 3, 4, 5}
+	slice := []int{1, 2, 3, 4, 5}
+
+	changeArray(array)
+	changeSlice(slice)
+
+	fmt.Println(array)
 	fmt.Println(slice)
 }
