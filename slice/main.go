576c3e323f0ceae943b810a91bbac632ea264900
root <duaehdtjs20@gmail.com>
2021-08-19 20:14:47 +0900

    slice - part 2 slicing

--- slice/main.go
+++ slice/main.go
@@ -2,21 +2,12 @@ package main
 
 import "fmt"
 
-func changeArray(array2 [5]int) {
-	array2[2] = 200
-}
-
-func changeSlice(slice2 []int) {
-	slice2[2] = 200
-}
-
 func main() {
-	array := [5]int{1, 2, 3, 4, 5}
-	slice := []int{1, 2, 3, 4, 5}
+	slice1 := []int{1, 2, 3, 4, 5}
+	slice2 := make([]int, len(slice1))
+	copy(slice2, slice1)
 
-	changeArray(array)
-	changeSlice(slice)
-
-	fmt.Println(array)
-	fmt.Println(slice)
+	slice2[1] = 100
+	fmt.Println(slice1)
+	fmt.Println(slice2)
 }
