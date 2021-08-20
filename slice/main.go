7faeb9d879163ed4d5366de3bbcb50770f93d5b5
root <duaehdtjs20@gmail.com>
2021-08-20 19:03:16 +0900

    slice - part 2 sorting

--- slice/main.go
+++ slice/main.go
@@ -1,13 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
+
+type Student struct {
+	Name string
+	Age  int
+}
+
+type Students []Student
+
+func (s Students) Len() int           { return len(s) }
+func (s Students) Less(i, j int) bool { return s[i].Age < s[j].Age }
+func (s Students) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 
 func main() {
-	slice1 := []int{1, 2, 3, 4, 5}
-	slice2 := make([]int, len(slice1))
-	copy(slice2, slice1)
-
-	slice2[1] = 100
-	fmt.Println(slice1)
-	fmt.Println(slice2)
+	s := []Student{
+		{"화랑", 31},
+		{"백두산", 52},
+		{"류", 42},
+		{"켄", 38},
+		{"송하나", 18},
+	}
+	sort.Sort(Students(s))
+	fmt.Println(s)
 }
